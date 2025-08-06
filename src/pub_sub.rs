dd78fae39d42a6d50f4cbcbe354e47943beee0b6
loveyoupeng <loveyoupeng@gmail.com>
2025-08-06 23:43:01 +0800

    const size

--- src/pub_sub.rs
+++ src/pub_sub.rs
@@ -1,26 +1,30 @@
 use std::marker::PhantomData;
 
-pub struct Publiser<T: Clone> {
+pub struct Buffer<T: Send, const N: usize> {
+    values: [Option<T>; N],
+}
+
+pub struct Publiser<T: Send> {
     _phantom: PhantomData<T>,
 }
-pub struct Subscriber<T: Clone> {
+pub struct Subscriber<T: Send> {
     _phantom: PhantomData<T>,
 }
 
-impl<T: Clone> Publiser<T> {
+impl<T: Send> Publiser<T> {
     pub fn publish(&self, _value: T) {}
 }
 
-impl<T: Clone> Subscriber<T> {
+impl<T: Send> Subscriber<T> {
     pub fn try_poll(&self) -> Option<T> {
         None
     }
 }
 
-unsafe impl<T: Clone> Send for Publiser<T> {}
-unsafe impl<T: Clone> Send for Subscriber<T> {}
+unsafe impl<T: Send> Send for Publiser<T> {}
+unsafe impl<T: Send> Send for Subscriber<T> {}
 
-pub fn create_buffer<T: Clone>(_: usize) -> (Publiser<T>, Subscriber<T>) {
+pub fn create_buffer<T: Send, const N: usize>() -> (Publiser<T>, Subscriber<T>) {
     (
         Publiser {
             _phantom: PhantomData,
