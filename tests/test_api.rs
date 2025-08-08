1b0c4b9e21ef27038befd4ceb2ba6332b98890b5
loveyoupeng <loveyoupeng@gmail.com>
2025-08-09 00:34:49 +0800

    channel test

--- tests/test_api.rs
+++ tests/test_api.rs
@@ -1,3 +1,5 @@
+use std::sync::mpsc::channel;
+
 use rust_design_patterns::{add, i32_getter};
 
 #[test]
@@ -11,4 +13,17 @@ fn test_fileds_macro() {
     let bytes: [u8; 4] = [0x03, 0x00, 0x00, 0x00];
     let value: i32 = i32_getter!(bytes);
     assert_eq!(3, value);
+}
+
+#[test]
+fn test_channel() {
+    let (tx, rx) = channel::<i32>();
+    match tx.send(10) {
+        Ok(_) => {}
+        Err(_) => unreachable!(),
+    }
+    match rx.recv() {
+        Ok(value) => assert_eq!(10, value),
+        Err(_) => unreachable!(),
+    }
 }
