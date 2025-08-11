4ee2af3c1af083b0ac46832eb19f8844239e7240
loveyoupeng <loveyoupeng@gmail.com>
2025-08-11 23:57:04 +0800

    perf test pub_sub

--- benches/perf_bench.rs
+++ benches/perf_bench.rs
@@ -1,5 +1,10 @@
 use criterion::{Criterion, criterion_group, criterion_main};
-use std::hint::black_box;
+use rust_design_patterns::pub_sub::create_buffer;
+use std::{
+    hint::black_box,
+    sync::{Arc, atomic::AtomicBool},
+    thread,
+};
 
 fn add(i: i32) -> i32 {
     i + i
@@ -9,5 +14,22 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("simple", |b| b.iter(|| add(black_box(5))));
 }
 
+fn pub_sub_benchmark(c: &mut Criterion) {
+    let (publisher, subscriber) = create_buffer::<i32>(1024);
+    let lock = Arc::new(AtomicBool::new(true));
+    let inner_lock = lock.clone();
+    let thread = thread::spawn(move || {
+        while inner_lock.load(std::sync::atomic::Ordering::Acquire) {
+            subscriber.try_poll();
+        }
+    });
+    c.bench_function("publish", |b| {
+        b.iter(|| while !publisher.try_offer(black_box(1)) {})
+    });
+    lock.store(false, std::sync::atomic::Ordering::Release);
+    thread.join().unwrap();
+}
+
 criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+criterion_group!(pub_sub, pub_sub_benchmark);
+criterion_main!(pub_sub);
